38cab835aab2bf34d9bd1c7ae8fe4cfb8d6e03b9
wong1125 <wong1125@naver.com>
2025-07-24 14:40:29 +0900

    [Feat] 플레이어 WASD 움직임

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movingSpeed = 5.0f;
+    
+    Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ArrowMove();
+    }
+
+    void ArrowMove()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 movingVector = new Vector2(horizontal, vertical) * movingSpeed;
+        rb.velocity = movingVector;
+    }
+}
